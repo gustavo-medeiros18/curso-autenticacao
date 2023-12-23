9595afef844ea3b746d2ad4f2e7cb99a914dd480
Gustavo Medeiros <gugamedeiros517@gmail.com>
2023-12-23 17:02:45 -0300

    :sparkles: feat: Update app.js with double quotes and add support for JSON parsing

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const bodyParser = require('body-parser');
+const bodyParser = require("body-parser");
 
 app.use(
   bodyParser.urlencoded({
-    extended: true
-  })
+    extended: true,
+  }),
+  bodyParser.json()
 );
 
 module.exports = app;
